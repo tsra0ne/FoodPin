98abfe5f2d025b435555c50b03410a14b3a7b04a
Sravan Goud <tsravangoud164@gmail.com>
2025-01-14 21:41:12 +0530

    Initial Commit

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Sravan Goud on 14/01/25.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,140 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Sravan Goud on 14/01/25.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+    
+    var restaurantNames = [
+        "Cafe Deadend",
+        "Homei",
+        "Teakha",
+        "Cafe Loisl",
+        "Petite Oyster",
+        "For Kee Restaurant",
+        "Po's Atelier",
+        "Bourke Street Bakery",
+        "Haigh's Chocolate",
+        "Palomino Espresso",
+        "Upstate",
+        "Traif",
+        "Graham Avenue Meats And Deli",
+        "Waffle & Wolf",
+        "Five Leaves",
+        "Cafe Lore",
+        "Confessional",
+        "Barrafina",
+        "Donostia",
+        "Royal Oak",
+        "CASK Pub and Kitchen"
+    ]
+    
+    var restaurantImages = [
+        "cafedeadend",
+        "homei",
+        "teakha",
+        "cafeloisl",
+        "petiteoyster",
+        "forkee",
+        "posatelier",
+        "bourkestreetbakery",
+        "haigh",
+        "palomino",
+        "upstate",
+        "traif",
+        "graham",
+        "waffleandwolf",
+        "fiveleaves",
+        "cafelore",
+        "confessional",
+        "barrafina",
+        "donostia",
+        "royaloak",
+        "cask"
+    ]
+    
+    var restaurantLocations = [
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Hong Kong",
+        "Sydney",
+        "Sydney",
+        "Sydney",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "New York",
+        "London",
+        "London",
+        "London",
+        "London"
+    ]
+    
+    var restaurantTypes = [
+        "Coffee & Tea Shop",
+        "Cafe",
+        "Tea House",
+        "Austrian / Causual Drink",
+        "French",
+        "Bakery",
+        "Bakery",
+        "Chocolate",
+        "Cafe",
+        "American / Seafood",
+        "American",
+        "American",
+        "Breakfast & Brunch",
+        "Coffee & Tea",
+        "Coffee & Tea",
+        "Latin American",
+        "Spanish",
+        "Spanish",
+        "Spanish",
+        "British",
+        "Thai"
+    ]
+    
+    lazy var dataSource = configureDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = dataSource
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(restaurantNames, toSection: .all)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    enum Section {
+        case all
+    }
+    
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+        let cellIdentifier = "dataCell"
+        
+        let dataSource = UITableViewDiffableDataSource<Section, String>(tableView: tableView) { tableView, indexPath, restaurantName in
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+            cell.nameLabel.text = restaurantName
+            cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+            cell.typeLabel.text = self.restaurantTypes[indexPath.row]
+            cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
+            return cell
+        }
+        
+        return dataSource
+    }
+
+}
